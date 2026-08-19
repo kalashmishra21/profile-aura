ce68b1022a24489c8ee36c038d36acc7d36de61d
Kalash Mishra <kalashji21@gmail.com>
2026-08-19 16:48:04 +0530

    fix: make snake wrapper idempotent

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -18,12 +18,12 @@ import { registerWrapCommand } from './commands/wrap.js';
 import { CacheService } from '../services/cache.js';
 
 const program = new Command();
-const VERSION = '4.0.5';
+const VERSION = '4.0.6';
 
 program
   .name('profile-aura')
   .description('Profile Aura 2.0 - SVG-First GitHub Profile Generator')
-  .version('4.0.5');
+  .version('4.0.6');
 
 registerBuildCommand(program);
 registerInitCommand(program);
